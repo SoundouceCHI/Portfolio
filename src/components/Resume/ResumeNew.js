cafc448ef6e1a0b53a02c9761861466ed529e3bb
unknown <soundouce.chibani@gmail.com>
2024-11-13 13:45:26 +0100

    update resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/soundouce_CHIBANI.pdf";
+import pdf from "../../Assets/../Assets/CV_Soundouce_CHIBANI.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
